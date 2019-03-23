a606c1f8cddafe0cf2d0d7c1457c30e84a5b6721
duckShooter <giovanni.n@gmx.us>
2019-03-23 06:27:18 +0200

    Added an index and controller for owners. closes #12

--- pet-clinic-web/src/main/java/guru/framework/petclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/guru/framework/petclinic/controllers/OwnerController.java
@@ -5,11 +5,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
-@RequestMapping("/vets") //Main sub-directory, any request mapping on method-level will be prefixed by that.
-public class VetController {
+@RequestMapping("/owners") //Main sub-directory, any request mapping on method-level will be prefixed by that.
+public class OwnerController {
 	
 	@RequestMapping(value={"", "/", "/index", "/index.html"}, method=RequestMethod.GET)
-	public String listVets() {
-		return "vets/index";
+	public String listOwners() {
+		return "owners/index";
 	}
 }
