9197f99a03a46488dd2be7e8167f75d374a80ab0
duckShooter <giovanni.n@gmx.us>
2019-03-27 17:49:15 +0200

    implemented Speciality map service .closes #24

---
+++ pet-clinic-data/src/main/java/guru/framework/petclinic/services/SpecialityService.java
@@ -0,0 +1,7 @@
+package guru.framework.petclinic.services;
+
+import guru.framework.petclinic.model.Speciality;
+
+public interface SpecialityService extends CrudService<Speciality, Integer> {
+
+}
---
+++ pet-clinic-data/src/main/java/guru/framework/petclinic/services/map/SpecialityMapService.java
@@ -0,0 +1,34 @@
+package guru.framework.petclinic.services.map;
+
+import java.util.Set;
+
+import guru.framework.petclinic.model.Speciality;
+import guru.framework.petclinic.services.SpecialityService;
+
+public class SpecialityMapService extends AbstractMapService<Speciality, Integer> implements SpecialityService {
+
+	@Override
+	public Set<Speciality> findAll() {
+		return super.findAll();
+	}
+
+	@Override
+	public Speciality findById(Integer id) {
+		return super.findById(id);
+	}
+
+	@Override
+	public Speciality save(Speciality object) {
+		return super.save(object);
+	}
+
+	@Override
+	public void deleteById(Integer id) {
+		super.deleteById(id);
+	}
+
+	@Override
+	public void delete(Speciality object) {
+		super.delete(object);
+	}
+}
