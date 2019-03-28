c41ee47a4055dd2b9e5486368dc2b6e846955c60
duckShooter <giovanni.n@gmx.us>
2019-03-28 16:59:26 +0200

    fixed broken links. closes #33

--- pet-clinic-web/src/main/java/guru/framework/petclinic/controllers/IndexController.java
+++ pet-clinic-web/src/main/java/guru/framework/petclinic/controllers/IndexController.java
@@ -1,12 +1,12 @@
 package guru.framework.petclinic.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class IndexController {
-	
 	@RequestMapping(value={"", "/", "index", "index.html"}, method=RequestMethod.GET)
 	public String index() {
 		return "index";
