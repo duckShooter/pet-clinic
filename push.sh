791bc4c798f938345e2feb5256356f239f2d85ae
duckShooter <giovanni.n@gmx.us>
2019-04-06 23:16:40 +0200

    add debug messages

--- push.sh
+++ push.sh
@@ -1,6 +1,10 @@
 # grant 'executable' permission with > chmod +x push.sh
 # run script with > ./push.sh "commit_msg"
+echo "#~>>>>>>>>>>>>>>> Checking Status <<<<<<<<<<<<<<<<<~#"
 git status
+echo "#~>>>>>>>>>>>>>>> Adding files <<<<<<<<<<<<<<<<<~#"
 git add .
+echo "#~>>>>>>>>>>>>>>> Committing <<<<<<<<<<<<<<<<<~#"
 git commit -m "${1}"
+echo "#~>>>>>>>>>>>>>>> Pushing <<<<<<<<<<<<<<<<<~#"
 git push
