6781b4d372dea1be11f061c461071b7d84741dbb
duckShooter <giovanni.n@gmx.us>
2019-04-18 03:33:37 +0200

    upgraded to JUnit5. closes #4

--- pet-clinic-web/src/test/java/guru/framework/petclinic/PetClinicApplicationTests.java
+++ pet-clinic-web/src/test/java/guru/framework/petclinic/PetClinicApplicationTests.java
@@ -1,16 +1,16 @@
 package guru.framework.petclinic;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 
-@RunWith(SpringRunner.class)
+@ExtendWith(SpringExtension.class)
 @SpringBootTest
 public class PetClinicApplicationTests {
 
 	@Test
 	public void contextLoads() {
+		
 	}
-
 }
